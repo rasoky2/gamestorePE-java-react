8f4bad39e3855dc8bce5c2312c52d68323abed78
rasoky2 <119987471+rasoky2@users.noreply.github.com>
2024-07-22 22:56:39 -0500

    Agregar carpetas backend, frontend y vscode y sistema Auth0

--- gamestorepe-backend/src/main/java/com/gamestore/config/SecurityConfig.java
+++ gamestorepe-backend/src/main/java/com/gamestore/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.gamestore.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+        @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+        private String jwkSetUri;
+
+        @Bean
+        public JwtDecoder jwtDecoder() {
+                return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+        }
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
